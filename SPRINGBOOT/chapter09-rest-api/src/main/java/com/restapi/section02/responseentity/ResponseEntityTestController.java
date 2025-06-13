46695b4396ce6268b09bc387060d13b20b73cf76
sieun <sieunuies@gmail.com>
2025-06-13 17:22:33 +0900

    [250613] TIL : springboot-rest api

---
+++ SPRINGBOOT/chapter09-rest-api/src/main/java/com/restapi/section02/responseentity/ResponseEntityTestController.java
@@ -0,0 +1,75 @@
+package com.restapi.section02.responseentity;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/entity")
+public class ResponseEntityTestController {
+    /*
+    [ResponseEntity]
+    결과 데이터와 HTTP 상태 코드를 직접 제어할 수 있는 클래스이다.
+    HttpStatus, HttpHeaders, HttpBody를 포함한다.
+    */
+
+    private List<UserDTO> users;
+
+    public ResponseEntityTestController() {
+        users = new ArrayList<>();
+        users.add(new UserDTO(1, "user01", "pass01", "다람쥐", new Date()));
+        users.add(new UserDTO(2, "user02", "pass02", "코알라", new Date()));
+        users.add(new UserDTO(3, "user03", "pass03", "판다", new Date()));
+    }
+
+    /* 회원 전체 조회 */
+    @GetMapping("/users")
+    public ResponseEntity<ResponseMessage> findAllUsers() {
+
+        /* 응답 헤더 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        /* 응답 데이터 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("users", users);
+
+        ResponseMessage responseMessage = new ResponseMessage(200, "조회 성공", responseMap);
+
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
+
+    /* 회원 상세 조회 */
+    @GetMapping("/users/{userNo}")
+    public ResponseEntity<ResponseMessage> findUserByNo(@PathVariable int userNo) {
+
+        /* 응답 헤더 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo)
+                .collect(Collectors.toList()).get(0);
+
+        /* 응답 데이터 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("user", foundUser);
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(200, "조회 성공", responseMap));
+    }
+    
+//    @PostMapping("/users")
+//    public ResponseEntity<?> registUser(@RequestBody UserDTO newUser) {
+//        System.out.println("newUser = " + newUser);
+//    }
+
+
+}
---
+++ SPRINGBOOT/chapter09-rest-api/src/main/java/com/restapi/section05/swagger/SwaggerTestController.java
@@ -0,0 +1,72 @@
+package com.restapi.section05.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Tag(name = "Spring Boot Swqgger 연동(user)")
+@RestController
+@RequestMapping("/swagger")
+public class SwaggerTestController {
+
+    private List<UserDTO> users;
+
+    public SwaggerTestController() {
+        users = new ArrayList<>();
+        users.add(new UserDTO(1, "user01", "pass01", "다람쥐", new Date()));
+        users.add(new UserDTO(2, "user02", "pass02", "코알라", new Date()));
+        users.add(new UserDTO(3, "user03", "pass03", "판다", new Date()));
+    }
+
+    /* 회원 전체 조회 */
+    @Operation(summary = "전체 회원 조회", description = "전체 회원 목록을 조회한다.")
+    @GetMapping("/users")
+    public ResponseEntity<ResponseMessage> findAllUsers() {
+
+        /* 응답 헤더 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        /* 응답 데이터 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("users", users);
+
+        ResponseMessage responseMessage = new ResponseMessage(200, "조회 성공", responseMap);
+
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
+
+    /* 회원 상세 조회 */
+    @Operation(summary = "회원 번호로 회원 조회", description = "회원 번호를 통해 해당하는 회원 번호를 조회한다.")
+    @GetMapping("/users/{userNo}")
+    public ResponseEntity<ResponseMessage> findUserByNo(@PathVariable int userNo) {
+
+        /* 응답 헤더 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo)
+                .collect(Collectors.toList()).get(0);
+
+        /* 응답 데이터 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("user", foundUser);
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(200, "조회 성공", responseMap));
+    }
+}
